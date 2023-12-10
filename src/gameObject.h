b824d22b4f74db80df3ad60251390ae6109742cf
Niko Korkala <nikokorkala5@kamk.fi>
2023-12-11 01:48:42 +0200

    delete contents of gameobject class

--- src/gameObject.h
+++ src/gameObject.h
@@ -3,44 +3,5 @@
 
 namespace KarhuRayTracer
 {
-	struct GameObject
-	{
-		//virtual ~GameObject() = 0;
-		/* Setters. */
-		void setPosition(glm::vec3& position);
-		void setScale(glm::vec3& scale);
 
-		/* Getters. */
-		glm::vec3 getPosition() { return m_Position; }
-		glm::vec3 getScale() { return m_Scale; }
-
-	private:
-		/* Member variables. */
-		glm::vec3 m_Position;
-		glm::vec3 m_Scale;
-		glm::mat4 m_ModelMatrix;
-		glm::mat4 m_ViewMatrix;
-	};
-
-	struct Box : public GameObject
-	{
-		Box(float width, float height);
-		~Box();
-		void resizeFramebuffer(float width, float height);
-		void render();
-		unsigned int m_VAO;
-		unsigned int m_VBO;
-		unsigned int m_EBO;
-		unsigned int m_FBO;
-		unsigned int m_RBO;
-		unsigned int fbotex;
-	};
-
-	struct Sphere : public GameObject
-	{
-
-		unsigned int m_VAO;
-		unsigned int m_VBO;
-		unsigned int m_EBO;
-	};
 }

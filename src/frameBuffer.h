f73eae94ec07082be6582d6592a84886a6a3cd3a
Niko Korkala <nikokorkala5@kamk.fi>
2023-12-11 01:54:10 +0200

    Make initalizations for framebuffers(not in use atm)

--- src/frameBuffer.h
+++ src/frameBuffer.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <cstdint>
 
 
 namespace KarhuRayTracer
@@ -9,8 +10,16 @@ namespace KarhuRayTracer
 		FrameBuffer();
 		~FrameBuffer();
 
+		void createFrameBuffer(uint32_t width, uint32_t height);
+		void createRenderBuffer(uint32_t width, uint32_t height);
+
 		void bind();
 		void unbind();
+
+		unsigned int getColorTExture() { return m_ColorTexture; }
 	private:
+		unsigned int m_FBO; /* FrameBuffer object. */
+		unsigned int m_RBO; /* RenderBuffer object. */
+		unsigned int m_ColorTexture;
 	};
 }
